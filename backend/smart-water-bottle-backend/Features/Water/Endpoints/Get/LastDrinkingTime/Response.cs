d3d3ffaf3ec2bf27646733d17e4acfdfce5a5658
Kevin Lingk <klingk.msg@gmail.com>
2025-06-06 10:25:46 +0200

    feat: add endpoint to get last drinking time

---
+++ backend/smart-water-bottle-backend/Features/Water/Endpoints/Get/LastDrinkingTime/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace smart_water_bottle_backend.Features.Water.Endpoints.Get.LastDrinkingTime;
+
+public class Response
+{
+    public DateTime? LastDrinkingTime { get; set; }
+    public int MinutesSinceLastDrink { get; set; }
+    public bool ShouldSendReminder { get; set; } 
+    public string? ReminderMessage { get; set; } 
+}
