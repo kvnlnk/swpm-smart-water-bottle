1887b8f1a7768ecec47d99fc963fadabdcfdc0ec
Kevin Lingk <klingk.msg@gmail.com>
2025-06-15 14:33:09 +0200

    feat: add simple water bottle (stole the code)

--- frontend/swpm_flutter_app/lib/pages/home.dart
+++ frontend/swpm_flutter_app/lib/pages/home.dart
@@ -1,31 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:supabase_auth_ui/supabase_auth_ui.dart';
+import '../components/water_bottle/water_bottle.dart';
 
-import '../constants.dart';
-
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  double waterLevel = 0.5;
+  int selectedStyle = 0;
+
+  @override
   Widget build(BuildContext context) {
+    final plain = WaterBottle(
+      waterColor: Colors.blue,
+      bottleColor: Colors.lightBlue,
+      capColor: Colors.blueGrey,
+      waterLevel: waterLevel,
+    );
+
+    final bottle = Center(
+      child: SizedBox(width: 200, height: 300, child: plain),
+    );
+
+    final waterSlider = Padding(
+      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(Icons.opacity),
+          SizedBox(width: 10),
+          Expanded(
+            child: Slider(
+              value: waterLevel,
+              max: 1.0,
+              min: 0.0,
+              onChanged: (value) {
+                setState(() {
+                  waterLevel = value;
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+
     return Scaffold(
-      appBar: appBar('Home'),
-      body: Center(
+      appBar: AppBar(
+        title: const Text(
+          "Smart Water Bottle",
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.white,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('You are home', style: TextStyle(fontSize: 42)),
-            ElevatedButton(
-              onPressed: () {
-                Supabase.instance.client.auth.signOut();
-                Navigator.of(context).pushReplacementNamed('/');
-              },
-              child: const Text(
-                'Log Out',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-            ),
-          ],
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [Spacer(), bottle, Spacer(), waterSlider, Spacer()],
         ),
       ),
     );
