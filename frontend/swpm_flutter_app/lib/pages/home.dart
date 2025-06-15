8f19c264f91f398806111c259beec490fd312d9c
Kevin Lingk <klingk.msg@gmail.com>
2025-06-15 16:30:24 +0200

    feat: get username and date format

--- frontend/swpm_flutter_app/lib/pages/home.dart
+++ frontend/swpm_flutter_app/lib/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../components/water_bottle/water_bottle.dart';
 
 class Home extends StatefulWidget {
@@ -10,7 +12,24 @@ class Home extends StatefulWidget {
 
 class HomeState extends State<Home> {
   double waterLevel = 0.5;
-  int selectedStyle = 0;
+  double dailyGoal = 2.5;
+  final supabase = Supabase.instance.client;
+  String? username;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUsername();
+  }
+
+  void _loadUsername() {
+    final user = Supabase.instance.client.auth.currentUser;
+    if (user != null) {
+      setState(() {
+        username = user.userMetadata?['username'];
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +44,32 @@ class HomeState extends State<Home> {
       child: SizedBox(width: 200, height: 300, child: plain),
     );
 
+    final waterDisplay = Container(
+      margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.grey[50],
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(color: Colors.grey[300]!),
+      ),
+      child: Column(
+        children: [
+          Text(
+            "${(waterLevel * dailyGoal).toStringAsFixed(1)}L",
+            style: const TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              color: Colors.black87,
+            ),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            "von ${dailyGoal.toStringAsFixed(1)}L Tagesziel",
+            style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+          ),
+        ],
+      ),
+    );
     final waterSlider = Padding(
       padding: EdgeInsets.symmetric(vertical: 5, horizontal: 40),
       child: Row(
@@ -51,9 +96,17 @@ class HomeState extends State<Home> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "Smart Water Bottle",
-          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        title: Column(
+          children: [
+            Text("Hallo $username!", style: const TextStyle(fontSize: 18)),
+            Text(
+              DateFormat('EEEE, dd. MMMM yyyy').format(DateTime.now()),
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ],
         ),
         centerTitle: true,
         backgroundColor: Colors.white,
@@ -65,7 +118,14 @@ class HomeState extends State<Home> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [Spacer(), bottle, Spacer(), waterSlider, Spacer()],
+          children: [
+            Spacer(),
+            bottle,
+            Spacer(),
+            waterSlider,
+            waterDisplay,
+            Spacer(),
+          ],
         ),
       ),
     );
