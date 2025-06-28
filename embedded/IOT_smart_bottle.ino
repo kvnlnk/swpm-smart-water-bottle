616981646682055ae97fa5bf66f811f3e38d9cc7
DerLimo <166646640+DerLimo@users.noreply.github.com>
2025-06-28 13:50:41 +0200

    feat: Sync time and create drink session
    - Create an drink session, as long as the sensor measures something - After three seconds the session is over and the data is sent to the app
    - sends {"syncRequest": true} every 2 seconds after connecting to a device
    - after recieving {"syncConfirmed": true, "timestamp"} the time is sent on the ESP32 and the microcontroller starts sending data

--- embedded/IOT_smart_bottle.ino
+++ embedded/IOT_smart_bottle.ino
@@ -12,7 +12,14 @@ const byte ledNone = 2;
 const byte ledNormal = 4;
 const byte ledImportant = 16;
 
+float sessionVolumeMl = 0.0;
+int noWaterCounter = 0; 
 int fuellrichtung = 1;
+
+bool isConnected = false;
+bool isSynched = false;
+unsigned long lastSyncAttempt = 0;
+const unsigned long syncInterval = 2000;
 
 ESP32Time rtc(0);  
 
@@ -25,8 +32,6 @@ BLECharacteristic* pCharacteristic;
 void IRAM_ATTR pulseCounter() {
   pulseCount++;
 }
-
-
 
 // // Klasse, welche BLE Callback für empfangene Daten betreibt
 class MyCallbacks : public BLECharacteristicCallbacks {
@@ -46,9 +51,30 @@ class MyCallbacks : public BLECharacteristicCallbacks {
       return;
     }
 
-    if (!doc.containsKey("DrinkReminderTyp")) return;
+if (doc["syncConfirmed"] == true && doc.containsKey("timestamp")) {
+  String timestamp = doc["timestamp"];
 
-    int typ = doc["DrinkReminderTyp"];
+  struct tm timeinfo;
+  if (sscanf(timestamp.c_str(), "%4d-%2d-%2dT%2d:%2d:%2d",
+             &timeinfo.tm_year, &timeinfo.tm_mon, &timeinfo.tm_mday,
+             &timeinfo.tm_hour, &timeinfo.tm_min, &timeinfo.tm_sec) == 6) {
+
+    timeinfo.tm_year -= 1900;  // struct tm erwartet Jahre seit 1900
+    timeinfo.tm_mon -= 1;      // struct tm: 0 = Jan
+
+    time_t epochTime = mktime(&timeinfo);  
+    rtc.setTime(epochTime);
+
+    isSynched = true;
+    Serial.println("Synchronisation erfolgreich.");
+  } else {
+    Serial.println("Fehler beim Parsen des Zeitstempels.");
+  }
+  return;
+}
+
+
+    int typ = doc["DrinkReminderType"];
 
     // Alle LEDs ausschalten
     digitalWrite(ledNone, LOW);
@@ -66,8 +92,10 @@ class MyCallbacks : public BLECharacteristicCallbacks {
       case 2:
         digitalWrite(ledImportant, HIGH);
         break;
+      case 3:
+        break;
       default:
-        Serial.println("Unbekannter DrinkReminderTyp");
+        Serial.println("Unbekannter DrinkReminderType");
         break;
     }
     
@@ -80,11 +108,16 @@ class MyCallbacks : public BLECharacteristicCallbacks {
 class MyServerCallbacks : public BLEServerCallbacks{
     void onConnect(BLEServer* pServer){
     Serial.println("Mit Client verbunden");
+    isConnected = true;
+    isSynched = false;
+    lastSyncAttempt = 0;
   }
   // Advertising bei Disconnect
   void onDisconnect(BLEServer* pServer){
     Serial.println("Client getrennt, starte Advertising");
     delay(500); //Sicherheitsdelay
+    isConnected = false;
+    isSynched = false;
     BLEDevice::startAdvertising();
   }
 };
@@ -133,6 +166,22 @@ void loop() {
   }
   lastButtonState = currentButton;
 
+
+  if (isConnected && !isSynched) {
+    if (millis() - lastSyncAttempt >= syncInterval) {
+      StaticJsonDocument<64> syncMsg;
+      syncMsg["syncRequest"] = true;
+      String syncOut;
+      serializeJson(syncMsg, syncOut);
+      pCharacteristic->setValue(syncOut.c_str());
+      pCharacteristic->notify();
+      Serial.println("SyncRequest gesendet: " + syncOut);
+      lastSyncAttempt = millis();
+    }
+    delay(100);
+    return;
+  }
+
   noInterrupts();
   int countedPulses = pulseCount;
   pulseCount = 0;
@@ -144,19 +193,29 @@ void loop() {
   float volumePerSecond = flowRate / 60.0;
   float volumeMl = volumePerSecond * 1000.0;
 
-  if (fuellrichtung == 1 && volumeMl != 0) {
+  if (fuellrichtung == 1 ) {
+    if (volumeMl > 0){
+        sessionVolumeMl += volumeMl;
+        noWaterCounter = 0;
+    } else{
+      noWaterCounter++;
+    }
     //BLE senden
-    StaticJsonDocument<64> doc;
-    doc["amountMl"] = volumeMl;
-    doc["timestamp"] = rtc.getTime("%Y-%m-%dT%H:%M:%S.000Z");
+    if (noWaterCounter >= 3 && sessionVolumeMl > 0 && isConnected && isSynched ) {
+      StaticJsonDocument<64> doc;
+      doc["amountMl"] = sessionVolumeMl;
+      doc["timestamp"] = rtc.getTime("%Y-%m-%dT%H:%M:%S.000Z");
 
-    String output;
-    serializeJson(doc, output);
+      String output;
+      serializeJson(doc, output);
 
-    pCharacteristic->setValue(output.c_str());
-    pCharacteristic->notify();
+      pCharacteristic->setValue(output.c_str());
+      pCharacteristic->notify();
 
-    Serial.print("Gesendet: ");
-    Serial.println(output);
+      Serial.print("Gesendet: ");
+      Serial.println(output);
+
+      sessionVolumeMl = 0;
+    }
   }
 }
