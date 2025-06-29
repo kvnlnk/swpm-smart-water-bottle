a3aae1b40167ce21553541beba0be8dd465c959c
Kevin Lingk <klingk.msg@gmail.com>
2025-06-29 19:29:59 +0200

    chore: remove unused water_data import from MainPage

--- frontend/swpm_flutter_app/lib/pages/main_page.dart
+++ frontend/swpm_flutter_app/lib/pages/main_page.dart
@@ -6,7 +6,6 @@ import 'package:swpm_flutter_app/pages/settings.dart';
 import 'package:swpm_flutter_app/services/bluetooth/ble_service.dart';
 import 'package:swpm_flutter_app/store/bluetooth_device_data.dart';
 import 'package:swpm_flutter_app/store/user_data.dart';
-import 'package:swpm_flutter_app/store/water_data.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
